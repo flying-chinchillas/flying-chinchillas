3bb6088c12cab470bebbb277be02d34527c4f1a9
monica <quanz2023@gmail.com>
2023-11-23 12:27:20 -0800

    login/sign up css

--- fc/src/components/Signup/Signup.jsx
+++ fc/src/components/Signup/Signup.jsx
@@ -34,54 +34,52 @@ const Signup = () => {
   return (
     <main >        
         <section>
-            <div>
-                <div>                  
-                    <h1> Flying Chinchillas </h1>                                                                            
-                    <form>                                                                                            
-                        <div>
-                            <label htmlFor="email-address">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                label="Email address"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}  
-                                required                                    
-                                placeholder="Email address"                                
-                            />
-                        </div>
+            <div className='signup-box'>
+                <div>
+                    <img src = {'https://firebasestorage.googleapis.com/v0/b/flying-chinchillas.appspot.com/o/Logo-flying-chinchilla.png?alt=media&token=04a409dc-c35b-4123-8f9f-d18112b2044a'} alt="Logo" className={"logo-image"} />
+                </div>                 
+                <h2> Sign up </h2>                                                                                                                                                         
+                <label className='signup-label'
+                    htmlFor="email-address"
+                >
+                    Email address
+                </label>
+                <input className='signup-input'
+                    type="email"
+                    label="Email address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}  
+                    required                                    
+                    //placeholder="Email address"                                
+                />
 
-                        <div>
-                            <label htmlFor="password">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                label="Create password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)} 
-                                required                                 
-                                placeholder="Password"              
-                            />
-                        </div>                                             
-                        
-                        <button
-                            type="submit" 
-                            onClick={onSubmit}                        
-                        >  
-                            Sign up                                
-                        </button>
-                                                                     
-                    </form>
-                   
-                    <p>
-                        Already have an account?{' '}
-                        <NavLink to="/login" >
-                            Sign in
-                        </NavLink>
-                    </p>                   
-                </div>
+                <label className='signup-label'
+                    htmlFor="password"
+                >
+                    Password
+                </label>
+                <input className='signup-input'
+                    type="password"
+                    label="Create password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} 
+                    required                                 
+                    //placeholder="Password"              
+                   />                                           
+                    
+                <button className='signup-button'
+                    type="submit" 
+                    onClick={onSubmit}                        
+                >  
+                    Sign up                                
+                </button> 
+                
+                <label className='signup-label'>
+                    Already have an account? {' '}
+                    <NavLink to="/login" >
+                        Sign in
+                    </NavLink> 
+                </label>                  
             </div>
         </section>
     </main>
