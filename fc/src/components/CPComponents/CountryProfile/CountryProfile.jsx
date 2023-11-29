3f5f61d928e00dd95e07d003f76be4bfdacd43d7
Julie Cao <caojh1@uci.edu>
2023-11-28 18:57:13 -0800

    Work in progess for country profile functionality

--- fc/src/components/CPComponents/CountryProfile/CountryProfile.jsx
+++ fc/src/components/CPComponents/CountryProfile/CountryProfile.jsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import CPMap from "../CPMap/CPMap";
 import countryInfo from '../../../countryInfo.json';
 import { useParams } from 'react-router-dom';
+import CountryIcon from "../../DashboardComponents/CountryIcon/CountryIcon";
 
 export default function CountryProfile() {
     const temp_reviews = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -12,6 +13,9 @@ export default function CountryProfile() {
     const countryData = countryInfo[formattedCountryName];
     const position = [parseFloat(countryData.lat), parseFloat(countryData.long)];
 
+    function handleClick(country) {
+        console.log('its happening o');
+      }
 
   return (
     <div className={"cp"}>
@@ -19,12 +23,16 @@ export default function CountryProfile() {
             <CPMap position={position}/>
             <div className={"overall-rating"}>
                 <div className={"o-r-header"}>
-                    <img src={"https://firebasestorage.googleapis.com/v0/b/flying-chinchillas.appspot.com/o/happy-chinchilla.png?alt=media&token=4cfaa0bf-1eb9-49ef-8612-41618227638c"} alt="Rating" className={"rating-image"}/>
+                    Rating: <img src={"https://firebasestorage.googleapis.com/v0/b/flying-chinchillas.appspot.com/o/happy-chinchilla.png?alt=media&token=4cfaa0bf-1eb9-49ef-8612-41618227638c"} alt="Rating" className={"rating-image"}/>
                 </div>
+                Flag: <CountryIcon countryCode={countryData.code}/>
+                Rating published date:
+                Country code: {countryData.code.toUpperCase()}
+                
             </div>
         </div>
         <div className={"reviews"}>
-            <div className={"country-name"}>Name of Country</div>
+            <div className={"country-name"}>{country}</div>
             <div className={"review-header"}>Reviews:</div>
             <div className={"review-display"}>
                 {temp_reviews.map((review) => (
@@ -37,9 +45,9 @@ export default function CountryProfile() {
         </div>
         <div className={"v-groups-display"}>
             {vulnerable_grps.map((grp) => (
-                <div className={"vulnerable-tab"} key={grp}>
-                    {grp}
-                </div>
+                <button className={"vulnerable-tab"} key={grp} onClick={() => handleClick(grp)}>
+                    {grp} 
+                </button>
                 )
             )}
         </div>
