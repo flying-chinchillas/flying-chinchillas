f7daee2bda7d8cc1e9debefe13c0d8d9387b234e
monica <quanz2023@gmail.com>
2023-12-08 12:13:27 -0800

    Editing the design of header's button(dashboard/home, profile, logout)

--- fc/src/components/HeaderComponents/HeaderSearch/HeaderSearch.jsx
+++ fc/src/components/HeaderComponents/HeaderSearch/HeaderSearch.jsx
@@ -2,12 +2,15 @@ import "./HeaderSearch.css";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import * as React from "react";
+import Logout from '../../Logout/Logout';
+import { useNavigate } from "react-router";
 import countryInfo from "../../../countryInfo.json";
 
 
 export default function HeaderSearch(props) {
     const [baseCountries] = React.useState(Object.keys(countryInfo).sort());
     const [searchTerm, setSearchTerm] = React.useState("");
+    const navigate = useNavigate();
 
     function search() {
         let newCountryList = [];
@@ -30,10 +33,23 @@ export default function HeaderSearch(props) {
             search(); 
         }
     }
+
+    function gotoProfile(){
+        navigate('/userprofile')
+    }
+
+    function gotoDashboard(){
+        navigate('/dashboard')
+    }
     
     return (
         <div className={"header-search"}>
+            <img src = {'https://firebasestorage.googleapis.com/v0/b/flying-chinchillas.appspot.com/o/Logo-flying-chinchilla.png?alt=media&token=04a409dc-c35b-4123-8f9f-d18112b2044a'} alt="Logo" className={"header-logo-image"} />
             <input type="text" placeholder="Search for a Country..." onChange={handleTermChange} onKeyUp={handleKeyPress.bind(this)}></input>
+            <button className={"dashboard-button"} onClick={() => gotoDashboard()}></button>
+            <button className={"profile-button"} onClick={() => gotoProfile()}></button>
+            <Logout />
+                
         </div>
     );
 }
